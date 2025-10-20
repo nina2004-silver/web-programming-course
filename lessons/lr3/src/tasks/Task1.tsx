97a48c5720cf5d2576d489c0bc34b76ef7193d59
nina2004-silver <ninaturulina4@gmail.com>
2025-10-20 13:04:42 +0700

    Сделала 5 заданий

--- lessons/lr3/src/tasks/Task1.tsx
+++ lessons/lr3/src/tasks/Task1.tsx
@@ -20,13 +20,13 @@ const products: Product[] = [
     id: 1,
     name: 'Наушники',
     price: 5990,
-    image: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=400&h=300&fit=crop'
+    image: 'https://www.yablochko.ru/upload/iblock/07e/airpods-max-hero-select-202011_FMT_WHH.jpg'
   },
   {
     id: 2,
     name: 'Смарт-часы',
     price: 12990,
-    image: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=400&h=300&fit=crop'
+    image: 'https://best-magazin.com/image/cache/catalog/product/Apple%20iWatch/SE%202/umnye-chasy-apple-watch-se-2-gps-44mm-starlight-%D1%81halk-pink-solo-loop-800x800.jpg'
   }
 ];
 
@@ -41,15 +41,15 @@ function Task1() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {products.map(product => (
-          <div key={product.id}>
-            {/* TODO: bg-white shadow-md rounded-lg p-4 hover:shadow-xl transition-shadow */}
-            <div>
-              {/* TODO: rounded w-full h-40 object-cover */}
-              <img src={product.image} alt={product.name} />
-              {/* TODO: text-lg font-bold mt-3 */}
-              <h3>{product.name}</h3>
-              {/* TODO: text-xl font-bold text-blue-600 mt-2 */}
-              <p>{product.price.toLocaleString('ru-RU')} ₽</p>
+          <div key={product.id} >
+
+            <div className='bg-white shadow-md rounded-lg p-4 hover:shadow-xl transition-shadow'>
+
+              <img src={product.image} alt={product.name} className="rounded w-full h-40 object-cover"/>
+
+              <h3 className="text-lg font-bold mt-3">{product.name}</h3>
+
+              <p className="text-xl font-bold text-blue-600 mt-2 ">{product.price.toLocaleString('ru-RU')} ₽</p>
             </div>
           </div>
         ))}
--- lessons/lr3/src/tasks/Task3.tsx
+++ lessons/lr3/src/tasks/Task3.tsx
@@ -7,12 +7,12 @@
  */
 
 const products = [
-  { id: 1, name: 'Ноутбук', price: 89990, rating: 4.8, image: 'https://images.unsplash.com/photo-1603302576837-37561b2e2302?w=300' },
-  { id: 2, name: 'Смартфон', price: 69990, rating: 4.7, image: 'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?w=300' },
-  { id: 3, name: 'Планшет', price: 45990, rating: 4.6, image: 'https://images.unsplash.com/photo-1561154464-82e9adf32764?w=300' },
-  { id: 4, name: 'Наушники', price: 25990, rating: 4.9, image: 'https://images.unsplash.com/photo-1546435770-a3e426bf472b?w=300' },
-  { id: 5, name: 'Часы', price: 18990, rating: 4.5, image: 'https://images.unsplash.com/photo-1579586337278-3befd40fd17a?w=300' },
-  { id: 6, name: 'Камера', price: 125990, rating: 4.9, image: 'https://images.unsplash.com/photo-1516035069371-29a1b244cc32?w=300' },
+  { id: 1, name: 'Ноутбук', price: 89990, rating: 4.8, image: 'https://mac77.ru/wp-content/uploads/02_apple-macbook-pro-16-2021-m1-max-big_ab9ed69fb681555d46885ee8fea3cab3.jpg' },
+  { id: 2, name: 'Смартфон', price: 69990, rating: 4.7, image: 'https://ipixel.ru/upload/iblock/8c2/bdr0cdorwugev12trznl6uvc9sb5blck.jpg' },
+  { id: 3, name: 'Планшет', price: 45990, rating: 4.6, image: 'https://my-apple-store.ru/wa-data/public/shop/products/04/14/21404/images/54650/54650.500@2x.jpg' },
+  { id: 4, name: 'Наушники', price: 25990, rating: 4.9, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9UOBLg0Ots1YkB3FENIoSsbtSk32Ef0XHbg&s' },
+  { id: 5, name: 'Часы', price: 18990, rating: 4.5, image: 'https://my-apple-store.ru/wa-data/public/shop/products/89/79/17989/images/41074/41074.500@2x.jpg'},
+  { id: 6, name: 'Камера', price: 125990, rating: 4.5, image: 'https://store.sony.ru/common/img/catalogue/312595/pxw-z150-1-big.png'}
 ];
 
 function Task3() {
@@ -25,13 +25,13 @@ function Task3() {
       </div>
 
       {/* TODO: grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 */}
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.map(p => (
           <div key={p.id} className="bg-white rounded-lg shadow-md p-4">
             <img src={p.image} alt={p.name} className="w-full h-40 object-cover rounded" />
             <h3 className="text-lg font-bold mt-3">{p.name}</h3>
             {/* TODO: hidden md:flex */}
-            <div className="mt-2 items-center gap-2">
+            <div className="mt-2 items-center gap-2 hidden md:flex">
               <span>⭐ {p.rating}</span>
             </div>
             <p className="text-xl font-bold text-blue-600 mt-2">{p.price.toLocaleString()} ₽</p>
