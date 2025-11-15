96719227772ed0cd0b4b3f08a0cfdaf21307d20c
Денис Песковацков <d.peskovackov@els24.com>
2025-11-15 19:55:17 +0700

    + tailwind support

--- lessons/lr5/src/App.tsx
+++ lessons/lr5/src/App.tsx
@@ -30,7 +30,9 @@ function App() {
       >
         Logout
       </button>
-      <h1>QWIZ GAME from lr4/src/Task4.tsx</h1> {/* <Task4 /> */}
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-2xl font-bold text-center">QUIZ GAME<br />from<br />lr4/src/Task4.tsx</h1> {/* <Task4 /> */}
+      </div>
     </>
   )
 }
